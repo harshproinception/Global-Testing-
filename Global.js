832a203256bf9aa37e04be983030ed64d5e37b6c
Ayushmaan <ayushmaan@proinception.com>
2026-09-07 13:05:14 +0530

    Update K2 styles

--- Global.js
+++ Global.js
@@ -4,22 +4,40 @@ function initializeRetalGlobalLayout() {
         $headerTarget = $('.form .row:first-child .root-table');
     }
     $headerTarget.addClass('retal-custom-header-layout');
- 
+
     var $sidebarRow = $('.form .row:has(a[name="Home btn"])');
     var $buttons = $sidebarRow.find('a.SourceCode-Forms-Controls-Web-Button');
-    if ($buttons.length && !$buttons.filter('.active-nav').length) {
-        $sidebarRow.find('a[name="Home btn"]').addClass('active-nav');
+
+    if ($buttons.length) {
+        var currentUrl = window.location.href.toLowerCase();
+        var activeButtonName = "Home btn";
+
+        if (currentUrl.indexOf("customer") !== -1 && currentUrl.indexOf("add") !== -1) {
+            activeButtonName = "Add custm btn";
+        } else if (currentUrl.indexOf("my") !== -1 && currentUrl.indexOf("customer" ) !==-1 || currentUrl.indexOf("all") !== -1 && currentUrl.indexOf("customer") !== -1    ) {
+            activeButtonName = "My custm btn";
+        } else if (currentUrl.indexOf("project") !== -1  ) {
+            activeButtonName = "Project btn";
+        } else if (currentUrl.indexOf("configuration") !== -1 ){
+            activeButtonName = "Configuration btn";
+        }  else if (currentUrl.indexOf("worklist") !== -1 ){
+            activeButtonName = "Worklist btn";
+        }
+
+        $buttons.removeClass('active-nav');
+        $sidebarRow.find('a[name="' + activeButtonName + '"]').addClass('active-nav');
     }
 }
- 
+
 $(document).ready(function () {
     initializeRetalGlobalLayout();
+
     $(document).on('click', '.form .row:has(a[name="Home btn"]) a.SourceCode-Forms-Controls-Web-Button', function () {
         $('.form .row:has(a[name="Home btn"]) a.SourceCode-Forms-Controls-Web-Button').removeClass('active-nav');
         $(this).addClass('active-nav');
     });
 });
- 
-$(document).ajaxComplete(function() {
+
+$(document).ajaxComplete(function () {
     initializeRetalGlobalLayout();
 });
